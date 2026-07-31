019fa7e1fb178c68784b9472210ccb12442d8546
Elaine <eeguia@gbox.adnu.edu.ph>
2026-07-31 18:52:17 +0800

    Create Problem3.cpp

---
+++ Diagnostic Assessment/Problem3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    
+    int arr[100];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
+    }
+    
+    int even = 0, odd = 0;
+    
+    for (int i = 0; i < n; i++){
+        if (arr[i] % 2 == 0){
+            even++;
+        } else{
+            odd++;
+        }
+    }
+    cout << "Even number count: " << even << endl; 
+    cout << "Odd number count: " << odd << endl;
+    
+    return 0;
+}
