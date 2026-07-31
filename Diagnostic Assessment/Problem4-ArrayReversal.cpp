3c42a844cb50467266cb45049cb2273db823a8d2
Elaine <eeguia@gbox.adnu.edu.ph>
2026-07-31 19:35:40 +0800

    Update Problem4-ArrayReversal.cpp

--- Diagnostic Assessment/Problem4-ArrayReversal.cpp
+++ Diagnostic Assessment/Problem4-ArrayReversal.cpp
@@ -8,12 +8,12 @@ int main() {
     
     int arr[100];
     cout << "Enter elements: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) { // iteration of the array elements
         cin >> arr[i];
     }
     
     cout << "Reversed: ";
-    for (int i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) { //used operation - to indicate going back or reversal
         cout << arr[i] << " ";
     }
     cout << endl;
