58199d37d5a582358766f6f544c641e6b149f248
Elaine <eeguia@gbox.adnu.edu.ph>
2026-08-19 19:15:02 +0800

    MatrixAddition.cpp

--- MatrixAddition.cpp
+++ MatrixAddition.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int matrixA[1000][1000];
+int matrixB[1000][1000];
+int sumMatrix[1000][1000]; //initialized outside of main for it to be accessible anywhere in the program and to handle large array
 
-return 0;
+int main() {
+  int r, c;
+  cin >> r >> c; //user input of rows and columns
+
+  for (int i = 0; i < r; i++) { //will read and perform operation for each row
+    for (int j = 0; j < c; j++) { //first column elements will be in MatrixA
+      cin >> matrixA[i][j];
+    }
+    for (int j = 0; j < c; j++) { //next column elements will be in MatrixB
+      cin >> matrixB[i][j];
+    }
+  }
+
+  for (int i = 0; i < r; i++) { //addition of the matrices
+    for (int j = 0; j < c; j++) {
+      sumMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+      cout << sumMatrix[i][j]; //printing of the sum of MatrixA and MatrixB
+
+      if (j < c - 1) {
+        cout << " "; //space to separate element results
+      }
+    }
+    cout << endl; //will read the next row iteration 
+  }
+    
+return 0; //termination once all rows are read and operations performed
 }
